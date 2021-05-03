030924316d63d0713fede2a1739021d1103a445c
Wilbert <winco117@gmail.com>
2021-05-03 18:56:55 -0500

    Actualización de aplicación con edición del curso de node 2021

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const hbs = require('hbs');
 
-require('./hbs/helpers');
-
+const app = express();
 const port = process.env.PORT || 3000;
+//require('./hbs/helpers');
 
-app.use(express.static(__dirname + '/public'));
-//Express HBS Engine
-hbs.registerPartials(__dirname + '/views/parciales');
+//Handelbars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/parciales');
+
+//Servir contenido estático
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'wilbert núñez'
+        nombre: 'Wilbert Núñez',
+        titulo: 'Curso de Node',
+        clase: ''
     });
 
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get('/generic', (req, res) => {
+    //res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+        nombre: 'Wilbert Núñez',
+        titulo: 'Curso de Node - Generic',
+        clase: 'alt'
+    });
 });
+
+app.get('/elements', (req, res) => {
+    //res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', {
+        nombre: 'Wilbert Núñez',
+        titulo: 'Curso de Node - elements',
+        clase: 'alt'
+    });
+});
+
+/*app.get('/about', (req, res) => {
+    res.render('about');
+});*/
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${port}`);
